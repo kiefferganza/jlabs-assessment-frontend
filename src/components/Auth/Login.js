ba7c90b38bc6081b39c50c9480a3d6d533fb103f
Kieffer Ganza <kiefferganza23@gmail.com>
2024-10-28 17:46:47 +0800

    added react router

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../../reducers/authReducer';
 
@@ -6,6 +6,7 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const error = useSelector((state) => state.auth.error);
 
   const handleLogin = () => {
@@ -31,7 +32,10 @@ function Login() {
         <button onClick={handleLogin} className="mt-6 bg-blue-500 text-white px-4 py-2 rounded-lg w-full">
           Login
         </button>
-        {error && <p className="text-red-500 mt-4">{error}</p>}
+          {/* Display error message if login fails */}
+          {error && <p className="text-red-500 mt-4">{error}</p>}
+
+{isAuthenticated && <p className="text-green-500 mt-4">You are logged in!</p>}
       </div>
     </div>
   );
